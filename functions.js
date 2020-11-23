194cb50ae503483c4f844c3e5d8cfece3ed60ec0
AndreiTirla <71433443+AndreiTirla@users.noreply.github.com>
2020-11-23 19:53:30 +0200

    show active menu

--- functions.js
+++ functions.js
@@ -17,11 +17,15 @@ function show(id) {
 
 function hidePreviousPage() {
     hide(activePage);
+    var link = document.querySelector(`#top-menu-bar a[data-page="${activePage}"]`)
+    link.classList.remove("active");
 }
 
 function showPage(pageId) {
     hidePreviousPage();
     show(pageId);
+    var link = document.querySelector(`#top-menu-bar a[data-page="${pageId}"]`)
+    link.classList.add("active");
     activePage = pageId;
 }
 
@@ -29,7 +33,7 @@ function initMenu() {
     document.addEventListener("click", function(e){
         var link = e.target;
         if(link.matches("#top-menu-bar a")) {
-            var id = link.innerHTML.toLowerCase();
+            var id = link.getAttribute("data-page");
             showPage(id);
         }
     })

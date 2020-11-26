bf46765702de54b1d1cab805dc5cb677e1cc64c7
AndreiTirla <71433443+AndreiTirla@users.noreply.github.com>
2020-11-26 21:49:11 +0200

    Sorted endorsements in descending order

--- functions.js
+++ functions.js
@@ -1,9 +1,9 @@
-var activePage= "skills";
+var activePage = "skills";
 
 function hide(id) {
     var el = document.getElementById(id);
-    
-    if(el) {
+
+    if (el) {
         el.style.display = "none";
     } else {
         console.error("Elementul nu exista");
@@ -12,7 +12,7 @@ function hide(id) {
 
 function show(id) {
     var elm = document.getElementById(id);
-    elm.style.display = ""; 
+    elm.style.display = "";
 }
 
 function hidePreviousPage() {
@@ -30,9 +30,9 @@ function showPage(pageId) {
 }
 
 function initMenu() {
-    document.addEventListener("click", function(e){
+    document.addEventListener("click", function (e) {
         var link = e.target;
-        if(link.matches("#top-menu-bar a")) {
+        if (link.matches("#top-menu-bar a")) {
             var id = link.getAttribute("data-page");
             showPage(id);
         }
@@ -43,20 +43,35 @@ initMenu();
 
 showPage(activePage);
 
+function sortSkills(skills) {
+    skills.sort(function (first, second) {
+        if (first.endorsements < second.endorsements) {
+            return 1;
+        }
+        if (first.endorsements > second.endorsements) {
+            return -1;
+        }
+        return 0
+    })
+}
+
 function showSkills(skills) {
-    var skillsLi = skills.map(function(skill){
+    var skillsLi = skills.map(function (skill) {
         var endorsements = ` <span>&middot; ${skill.endorsements}</span>`;
         return "<li>" + skill.name + endorsements + "</li>";
     });
-    
+
     //TODO add "favorite" skill
     var ul = document.querySelector("#skills ul");
     ul.innerHTML = skillsLi.join("");
 }
 
-fetch("data/skills.json").then(function(r) {
-    return r.json();
-}).then(function(allSkills) {
-    showSkills(allSkills);
-});
+fetch("data/skills.json")
+    .then(function (r) {
+        return r.json();
+    })
+    .then(function (allSkills) {
+        sortSkills(allSkills)
+        showSkills(allSkills)
+    });
 
